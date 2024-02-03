fcba07ff78d0ae1b37db11eea8935162375b53cd
aditya4-4 <adityadurgesh4@gmail.com>
2024-02-03 20:37:06 +0530

    cryptoTracker dashboard project done

--- src/components/Dashboard/Grid/index.jsx
+++ src/components/Dashboard/Grid/index.jsx
@@ -1,12 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-
+import { useState, useEffect } from "react";
 import TrendingUpRoundedIcon from "@mui/icons-material/TrendingUpRounded";
 import TrendingDownRoundedIcon from "@mui/icons-material/TrendingDownRounded";
+import StarsRoundedIcon from "@mui/icons-material/StarsRounded";
 
 import "./styles.css";
 import { Link } from "react-router-dom";
 
 function Grid({ coin }) {
+  const [allCoinId, setAllCoinId] = useState([]);
+
+  // console.log("coin data from grid",coin)
+
+  const handleOnWatchList = ( coinId) => {
+    if (!allCoinId.includes(coinId)) {
+      setAllCoinId((previousCoinId) => [...previousCoinId,coinId]);
+      console.log("allcoin id", allCoinId);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("watchList")) {
+      const watchListCoin = JSON.parse(localStorage.getItem("watchList"));
+      console.log("watchList", watchListCoin);
+      setAllCoinId(watchListCoin);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("allCoinId", allCoinId);
+    localStorage.setItem("watchList", JSON.stringify(allCoinId));
+  }, [allCoinId]);
+
   return (
     <Link to={`/coin/${coin.id}`}>
       <div
@@ -21,6 +47,15 @@ function Grid({ coin }) {
           <div className="name-col">
             <p className="coin-symbol">{coin.symbol}</p>
             <p className="coin-name">{coin.name}</p>
+          </div>
+          <div className="watchlist-icon">
+            <StarsRoundedIcon
+              sx={{ color: "#3a80e9" }}
+              onClick={(event) => {
+                event.preventDefault();
+                handleOnWatchList( coin.id);
+              }}
+            />
           </div>
         </div>
 
--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,12 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
+
 import Header from "../components/Common/Header";
+import TabsComponents from "../components/Dashboard/Tabs";
+import { get100Coins } from "../functions/get100Coins";
 
 function WatchList() {
+   const [allCoinId, setAllCoinId] = useState([])
+   const [allCoin, setAllCoin] = useState([]);
+   const [watchListCoinInfo, setWatchListCoinInfo] = useState([]);
+
+  useEffect(() => {
+    console.log("localStorage",localStorage)
+    const watchListCoinId =JSON.parse(localStorage.getItem('watchList'))
+    if(watchListCoinId){
+      console.log("watchListCoinId",watchListCoinId)
+      setAllCoinId(watchListCoinId);
+
+    }
+  },[])
+
+  useEffect(() =>{
+    getData();
+  },[])
+
+  const getData = async () => {
+    const coinData = await get100Coins();
+    if(coinData){
+      setAllCoin(coinData);
+      if(allCoin && allCoinId){
+        allCoinId.forEach((coinId) => {
+          const filteredCoin = allCoin.filter((coin) => coin.id == coinId);
+          setWatchListCoinInfo([...watchListCoinInfo, filteredCoin])
+        })
+      }
+    }
+  }
+
+
+
   return (
     <div className="watchlist-container">
       <Header />
-      <div className="empty-watchlist">
-        <h1>WatchList is Empty. Go to DashBoard</h1>
-      </div>
+      <TabsComponents coins={watchListCoinInfo}/>
     </div>
   );
 }
